5144e50e77f682ad6bfddc7ad4c7f33a86dcae7c
svyat <svyat@LAPTOP-JB283VOT>
2023-01-02 19:11:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[10];
+    int b[10];
+    int c[10];
+
+    int i;
+
+    cout << "1 massiv " << endl;
+    for (i = 0; i < 5; i++) {
+        cout << "[" << i + 1 << "]" << ": ";
+        cin >> a[i];
+    }
+
+    cout << "2 massiv" << endl;
+    for (i = 0; i < 5; i++) {
+        cout << "[" << i + 1 << "]" << ": ";
+        cin >> b[i];
+    }
+
+    for (i = 0; i < 5; i++) {
+        c[i] = a[i] + b[i];
+        cout << c[i] << " ";
+    }
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
